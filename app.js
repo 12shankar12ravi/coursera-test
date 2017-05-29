22cb1e080c15fb1d934815825be171942fc6c459
12shankar12ravi <12shankar12ravi@gmail.com>
2017-05-29 23:30:52 +0530

    Solution to the Problem

---
+++ app.js
@@ -0,0 +1,20 @@
+(function(){
+
+angular.module("MyApp",[])
+.controller("myController",myController);
+
+myController.$inject=['$scope'];
+function myController($scope){
+  $scope.checkIfTooMuch=  function (){
+        var items=($scope.items).split(",");
+        if(items.length<=3 && items[0]!="")
+          $scope.items="Enjoy!";
+        else if(items.length==1 && items[0]=="")
+          $scope.items="Please enter data first";
+        else
+          $scope.items="Too Much!";
+  }
+}
+
+
+})();
